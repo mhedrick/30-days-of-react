0dafc8b0eb9cfe88e88c67f746687b2a364ef3f2
Maggie Hedrick <maggiekhedrick@gmail.com>
2021-12-09 13:44:44 -0500

    turn index page into list

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,23 +6,36 @@ import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import { Link } from "gatsby"
 
-
 import day03_thumbnail from '../assets/day03_thumbnail.png';
+
+const days = [
+  {
+    number: "03",
+    title: "Setting Up",
+    image: day03_thumbnail,
+    description: "Putting together the React/Gatsby environment and getting back into the swing of things."
+  }
+];
 
 // markup 
 const IndexPage = () => {
   return (
-      <Layout>
-        <Row>
+    <Layout>
+      {days.map(({ number, title, image, description }) => (
+        <Row key={number}>
           <Col md="2">
-          <Link to="Day03"><Image src={day03_thumbnail} fluid thumbnail /></Link>
+            <Link to={`Day${number}`}><Image src={image} fluid thumbnail /></Link>
           </Col>
           <Col>
-          <h2><Link href="Day03">Day 03 <small className="text-muted">Setting Up</small></Link></h2>
-            Putting together the React/Gatsby environment and getting back into the swing of things.
+            <h2>
+              <Link to={`Day${number}`}>Day {number} <small className="text-muted">{title}</small></Link>
+            </h2>
+            {description}
           </Col>
         </Row>
-      </Layout>
+      ))}
+
+    </Layout>
   )
 }
 
